8d8f3ae9f70fc30d93bd500051e619b3e7053b13
Prateek Saraswat <prateeksaraswat@ACKO594.local>
2020-01-11 02:21:13 +0530

    Update 1.1

--- Graph/kruskal.cpp
+++ Graph/kruskal.cpp
@@ -2,7 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+pair<int,pair<int,int>> p[10005];
 int arr[10005];
+int n,m;
 
 //Disjoint set
 void initialize() {
@@ -13,8 +15,43 @@ void initialize() {
 
 int root(int x) {
 	while(arr[x]!=x) {
-		arr[x]=arr[arr[x]];  
+		arr[x]=arr[arr[x]];  //While computing the root of A, set each i to point to its grandparent (thereby halving the path length), where i is the node which comes in between path, while computing root of A.
 		x=arr[x];
 	}
 	return x;
+}
+
+void union1(int x,int y) {
+	int root_x=root(x);
+	int root_y=root(y);
+
+	arr[root_x] = root_y   //setting parent of root_x as root_y
+}
+
+int kruskal() {
+	int x,y,wt;
+	int min_cost=0;
+	for(int i=0;i<m;i++) {
+		x=p[i].second.first;
+		y=p[i].second.second;
+		wt=p[i].first;
+
+		if(root(x)!=root(y)) {
+			union1(x,y);
+			min_cost+=wt;
+		}
+	}
+	return min_cost;
+}
+
+int main() {
+	cin>>n>>m;
+
+	for(int i=0;i<m;i++) {
+		int x,y,wt;
+		cin>>x>>y>>wt;
+		p[i]=make_pair(wt,make_pair(x,y));
+	}
+	sort(p,p+m);
+	cout << kruskal();
 }
